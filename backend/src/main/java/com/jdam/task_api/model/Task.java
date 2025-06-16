eb903baaeb02f27f78d992ef546305cdb4a1cf69
Jdam <jonathandalvarez7@gmail.com>
2025-06-16 11:38:42 -0500

    i've added Task entity

---
+++ backend/src/main/java/com/jdam/task_api/model/Task.java
@@ -0,0 +1,27 @@
+package com.jdam.task_api.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer Id;
+    private String title;
+    private String description;
+    private Date date;
+    private Boolean done;
+}
