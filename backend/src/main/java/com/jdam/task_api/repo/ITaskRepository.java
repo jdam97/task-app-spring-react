683de68cf8d1c631bd6415766233b8af4838036c
Jdam <jonathandalvarez7@gmail.com>
2025-06-16 12:02:13 -0500

    repository interface

---
+++ backend/src/main/java/com/jdam/task_api/repo/ITaskRepository.java
@@ -0,0 +1,7 @@
+package com.jdam.task_api.repo;
+
+import com.jdam.task_api.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ITaskRepository extends JpaRepository<Task,Integer> {
+}
