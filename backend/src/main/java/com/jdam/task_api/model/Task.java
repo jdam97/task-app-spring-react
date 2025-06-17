e41a16d7f9ebe3961c6cf73b851f993c3a34ed32
Jdam <jonathandalvarez7@gmail.com>
2025-06-17 12:07:23 -0500

    Add Task entity and service interface for database queries

--- backend/src/main/java/com/jdam/task_api/model/Task.java
+++ backend/src/main/java/com/jdam/task_api/model/Task.java
@@ -8,8 +8,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-
 import java.util.Date;
+
 
 @Entity
 @Data
--- backend/src/main/java/com/jdam/task_api/repo/ITaskRepository.java
+++ backend/src/main/java/com/jdam/task_api/repo/ITaskRepository.java
@@ -2,6 +2,8 @@ package com.jdam.task_api.repo;
 
 import com.jdam.task_api.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface ITaskRepository extends JpaRepository<Task,Integer> {
+    public List<Task> findByTitleContainingIgnoreCase(String title);
 }
