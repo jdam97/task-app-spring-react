020b80d9e3614de23e53e3437383819685ae248c
Jdam <jonathandalvarez7@gmail.com>
2025-07-01 11:03:36 -0500

    feat: :sparkles: add tasklist

---
+++ frontend/src/components/TaskList.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { FaCalendarAlt } from 'react-icons/fa';
+import CardTask from './CardTask';
+
+interface TaskListProps {
+    data: any[];
+    state: String;
+}
+
+
+function TaskList({ data }: TaskListProps) {
+    
+    return (
+        <div className='p-7 '>
+            {(data.length>0) ?
+                <div className='flex flex-col gap-2'>
+                    {data.map((task,index)=>(
+                        <CardTask
+                        key={task.id}
+                        id={task.id}
+                        title={task.title}
+                        description={task.description}
+                        date={task.date}
+                        done={task.done}
+                        />
+                    ))}
+                </div>
+                :
+                <div className=' bg-white rounded-lg shadow-md'>
+                    <div className='flex flex-col items-center p-8 gap-2'>
+                        <FaCalendarAlt className='w-12 h-12 text-gray-400' />
+                        <p className=' text-gray-400'>No hay tareas en esta categoría</p>
+                    </div>
+
+                </div>
+
+
+            }
+        </div>
+    )
+}
+export default TaskList;
