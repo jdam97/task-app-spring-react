17716256ee52438fffb8c260bb9de56ceadf9694
Jdam <jonathandalvarez7@gmail.com>
2025-07-01 11:02:07 -0500

    feat: :sparkles: add cards info component

---
+++ frontend/src/components/CardsInfo.tsx
@@ -0,0 +1,27 @@
+
+
+
+interface CardsInfoProps {
+    title: string;
+    icon: React.ReactNode;
+    value: number;
+    valueClassName: string;
+}
+
+function CardsInfo ({title,icon,value,valueClassName}:CardsInfoProps) {
+
+    return (
+        <div className="flex flex-row bg-white rounded-lg shadow-md p-4 w-xs items-center justify-between">
+            <div className="flex flex-col">
+                <h4>{title}</h4>
+                <p className={`font-bold text-3xl ${valueClassName}`}>{value}</p>
+            </div>
+            <div>
+                {icon}
+            </div>
+        </div>
+    )
+  
+}
+
+export default CardsInfo
